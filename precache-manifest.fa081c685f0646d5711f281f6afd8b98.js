d092457e9eebc5ba48040f7231debd8231616df9
Adam Norstrom <adamnorstrom@berkeley.edu>
2022-09-08 18:21:42 -0700

    Updates

---
+++ precache-manifest.fa081c685f0646d5711f281f6afd8b98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77ff9438fd8ed7175487",
+    "url": "/Portfolio/static/css/main.dd69adc6.chunk.css"
+  },
+  {
+    "revision": "77ff9438fd8ed7175487",
+    "url": "/Portfolio/static/js/main.77ff9438.chunk.js"
+  },
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.591eb5be.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "be17ad977ad36e8482839ce58fb44fce",
+    "url": "/Portfolio/index.html"
+  }
+];
