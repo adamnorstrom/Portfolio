fd9e862850bbeab826fa4f7035211380ab54faf9
Adam Norstrom <adamnorstrom@berkeley.edu>
2022-09-17 00:51:34 -0700

    Updates

--- precache-manifest.68d70b26c9567cc7279e2abd81b1ddea.js
+++ precache-manifest.68d70b26c9567cc7279e2abd81b1ddea.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "a42fedf7733e52eabb7b",
+    "revision": "cb3b3023665402fa2326",
     "url": "/Portfolio/static/css/main.dd69adc6.chunk.css"
   },
   {
-    "revision": "a42fedf7733e52eabb7b",
-    "url": "/Portfolio/static/js/main.a42fedf7.chunk.js"
+    "revision": "cb3b3023665402fa2326",
+    "url": "/Portfolio/static/js/main.cb3b3023.chunk.js"
   },
   {
     "revision": "591eb5beb99e69ed8d3e",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/Portfolio/static/js/2.5ec21d73.chunk.js"
   },
   {
-    "revision": "22095027f2f166b9f30edc73436f17d9",
+    "revision": "a901cb0d8d43c7039467bdd0abb71a81",
     "url": "/Portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.24eb48c0a2ee721f4db3a2ca9f7f8e38.js"
+  "/Portfolio/precache-manifest.68d70b26c9567cc7279e2abd81b1ddea.js"
 );
 
 workbox.clientsClaim();
